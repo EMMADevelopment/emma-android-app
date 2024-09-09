d4baa8e41a2a1cec76dcfce6c4270a542c8c267d
Salva Moreno <salvador.moreno@ext.arkana.io>
2024-09-09 14:06:19 +0200

    Add Button model & button composable

---
+++ EmmaExampleApp/app/src/main/java/com/emma/emmaandroidexample/domain/Button.kt
@@ -0,0 +1,6 @@
+package com.emma.emmaandroidexample.domain
+
+data class Button(
+    val title: String,
+    val active: Boolean
+)
---
+++ EmmaExampleApp/app/src/main/java/com/emma/emmaandroidexample/ui/components/Button.kt
@@ -0,0 +1,43 @@
+package com.emma.emmaandroidexample.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.emma.emmaandroidexample.ui.theme.EmmaDark
+import com.emma.emmaandroidexample.ui.theme.EmmaLight
+
+@Composable
+fun Button(active: Boolean, text: String) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .height(44.dp)
+            .fillMaxWidth()
+            .background(if (active) EmmaDark else EmmaLight)
+    ) {
+        Text(
+            text.uppercase(),
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun Button_Preview() {
+    Column {
+        Button(active = true, text = "Start session")
+        Button(active = false, text = "Register user")
+    }
+}
