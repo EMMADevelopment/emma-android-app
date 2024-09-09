8d93d83e677ddf2f26cbabe9cf72212c8854fb99
Salva Moreno <salvador.moreno@ext.arkana.io>
2024-09-09 11:17:44 +0200

    Add EMMA colors

--- EmmaExampleApp/app/src/main/java/com/emma/emmaandroidexample/ui/theme/Color.kt
+++ EmmaExampleApp/app/src/main/java/com/emma/emmaandroidexample/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Emma
+val EmmaDark = Color(red = 72, green = 160, blue = 104)
+val EmmaMedium = Color(red = 93, green = 178, blue = 134)
+val EmmaLight = Color(red = 146, green = 206, blue = 179)
